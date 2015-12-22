b07653f050f1edc824ebc00606a096d103ad31fa
RoyTimes <roytimes@icloud.com>
2015-12-23 03:42:42 +0800

    Add improvement

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# auto_cmd
-useful shell script I use
+## A bunch of useful shell scripts
+there are the scripts that I use a lot. I wrote them down and keep them here.
+
+### What's inside 
+1. MongoDB scripts. (start with options & Auto backup script)
--- mongodb/auto_backup.sh
+++ mongodb/auto_backup.sh
@@ -1,6 +1,27 @@
-#!/bin/sh
+#!bin/sh
 
-# All rights reserved 2015 @zs (github.com/RoyTimes)
+# The MIT License (MIT)
+
+# Copyright (c) 2015 Song Zhou
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 
 DUMP=/usr/local/mongodb/bin/mongodump
 OUT_DIR=/home/backup/mongodb/mongod_bak_now
